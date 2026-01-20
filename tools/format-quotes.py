1c99167da3af8231c40a4dfea4858d5756c8b87d
Calvin Neo <calvinneo1995@gmail.com>
2026-01-21 01:37:55 +0800

    Site updated: 2026-01-21 01:37:53

--- tools/format-quotes.py
+++
@@ -1,7 +0,0 @@
-
-with open("../_posts/from-ym.md", "r", encoding='utf-8') as f:
-    txt = f.read()
-    txt = txt.replace("‘", "'")
-    txt = txt.replace("’", "'")
-    with open("../_posts/1--from-ym.md", "w") as fw:
-        fw.write(txt)
